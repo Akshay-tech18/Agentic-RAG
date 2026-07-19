d87d2f8ac623acb6e2412c46df3a2655d07404eb
Akshay R P <akshayrp04@gmail.com>
2026-07-19 22:11:35 +0530

    day 2 of learning RAG

--- app.py
+++ app.py
@@ -12,19 +12,106 @@
 
 # print(response["message"]["content"])
 
+# from ollama import chat
+
+# question = input("Ask me anything: ")
+
+# response = chat(
+#     model="qwen2.5-coder:7b",
+#     messages=[
+#         {
+#             "role":"user",
+#             "content": question
+#         }
+#     ]
+# )
+
+# print("\nAI:\n")
+# print(response["message"]["content"])
+
+# from ollama import chat
+
+# messages = []
+
+# print("=" * 40)
+# print("     Local AI assistant")
+# print("=" * 40)
+
+# while True:
+#     question = input("\n You: ")
+
+#     if question.lower() == "exit":
+#         print("\nGoodBye!")
+#         break
+
+#     messages.append(
+#         {
+#             "role":"user",
+#             "content": question
+#         }
+#     )
+
+#     response = chat(
+#         model="qwen2.5-coder:7b",
+#         messages=messages
+#     )
+
+#     answer = response["message"]["content"]
+#     print("\nAI: ")
+#     print(answer)
+
+#     messages.append(
+#         {
+#             "role":"assistant",
+#             "content": answer
+#         }
+#     )
+
 from ollama import chat
 
-question = input("Ask me anything: ")
+messages=[
+    {
+        "role":"system",
+        "content":"""
+you are an AI teacher.
+explain concepts simply.
+use real-life examples.
+keep answer under 150 words.
+if appropriate, end with one short quize question.
+"""
+    }
+]
 
-response = chat(
-    model="qwen2.5-coder:7b",
-    messages=[
+print("=" * 50)
+print("     AI teacher")
+print("=" * 50)
+
+while True:
+    question = input("\nYou: ")
+
+    if question.lower() == "exit":
+        break
+
+    messages.append(
         {
             "role":"user",
-            "content": question
+            "content":question
         }
-    ]
-)
+    )
 
-print("\nAI:\n")
-print(response["message"]["content"])
+    response = chat(
+        model="qwen2.5-coder:7b",
+        messages=messages
+    )
+
+    answer = response["message"]["content"]
+
+    print("\n Teacher \n")
+    print(answer)
+
+    messages.append(
+        {
+            "role":"user",
+            "content":answer
+        }
+    )
